e66333aa343559354943255a2a8adac106c3ce21
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-01-18 14:16:22 -0300

    Variaveis em PHP

---
+++ fundamentos/variaveis.php
@@ -0,0 +1,15 @@
+<?php
+
+$nomeCompleto = "Thiago César dos Reis Oliveira";
+$primeiroNome = "Thiago";
+$ultimoNome = "Oliveira";
+
+echo "<h2>$nomeCompleto</h2>";
+echo $primeiroNome . " " . $ultimoNome;
+echo "</br>";
+
+var_dump($primeiroNome);
+echo "</br>";
+var_dump($ultimoNome);
+echo "</br>";
+var_dump($nomeCompleto);
