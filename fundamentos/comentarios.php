e4e791fa6b3573586983ab0c9e223250f5451dca
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-01-18 12:52:35 -0300

    Fundamentos do PHP

---
+++ fundamentos/comentarios.php
@@ -0,0 +1,17 @@
+<h3>Comentarios</h3>
+<?php
+  
+//Comentário de linha
+/*
+* Comentário Destacado
+! Comentário de Alerta (Depreciação, etc)
+? Dúvida, questão, etc
+TODO: a fazer
+@param  parametro: o paramêtros para os métodos
+*/
+
+/*
+  Comentário de bloco
+  Múltiplas Linhas
+*/
+
