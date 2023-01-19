e0546991fbecab1b49a95ab16a5ce07f939f7d32
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-01-19 12:00:03 -0300

    Tipos de dados em PHP

---
+++ fundamentos/tipos-de-dados.php
@@ -0,0 +1,33 @@
+<?php
+
+$nome = 'Thiago César dos Reis Oliveira';
+
+// echo gettype($nome);
+echo gettype($nome);
+echo get_debug_type($nome);
+
+/* 
+* Tipos Escalares
+
+* Boolean bool
+* Integer int
+* Float (float - double) 
+* String (string)
+
+* Tipos Compostos
+
+* Array (array)
+* Object (object)
+* Callable  (callable)
+* Iterable (iterable)
+
+* Tipos Especiais
+
+* Resource (resource)
+* Null (null)
+
+*Tipos Pseudos 
+
+*Mixed (mixed)  
+*Void (void)
+*/
