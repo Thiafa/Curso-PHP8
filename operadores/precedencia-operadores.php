7693d05586ab57e68e5667abb53c8a8919fb145b
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-01-26 16:36:18 -0300

    Operadores de precedências

---
+++ operadores/precedencia-operadores.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<h1>Operadores de Precedências</h1>";
+
+// () -> clone new -> [ -> ** -> ++, --, ~, (int), (float),(string),(array),(object),(bool),@ , instanceof, !, *, /, %, ||, ??, ? :, = += -= *= **= /= .= %= &= |= ^= <<= >>=, and, xor, or
+
+echo 3+5*3 ;
+echo "<br>";
+echo (3+5)*3;
