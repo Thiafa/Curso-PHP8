e04f96f238de6dcd4f57d18db1329c561b657ffe
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-01-30 21:07:04 -0300

    Operadores Lógicos em PHP

---
+++ operadores/operadores-logicos.php
@@ -0,0 +1,20 @@
+<?php
+  echo "<h1 class='ativo'>Operadores Lógicos</h1>";
+
+$a = true;
+$b = true;
+$c = false;
+$espaco = "<\br>";
+
+var_dump($a and $b);
+echo "</br>";
+var_dump($a or $b);
+echo "</br>";
+var_dump($a xor $b);
+echo "</br>";
+var_dump(!$a);
+echo "</br>";
+var_dump($a && $b);
+echo "</br>";
+var_dump($a || $b);
+echo "</br>";
