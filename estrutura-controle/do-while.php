91c885a1b3f4e0b13bd9e41c1617e19fa17d723c
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-02-10 11:32:17 -0300

    Estrutura de controle - Do wWhile

---
+++ estrutura-controle/do-while.php
@@ -0,0 +1,17 @@
+<?php
+echo "<h1>Operador de Controle - Do While</h1>";  
+
+$a = 1;
+
+do{
+  echo "</br>$a";
+  $a += 1;
+}while ($a <= 10);
+
+echo "</br>-----------";
+$a = 1;
+
+while($a <= 10){
+  echo "</br>$a";
+  $a += 1;
+}
