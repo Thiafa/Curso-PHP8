effc7f2d44d0e07432ecffdbc088bee52a1d5b03
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-02-22 11:13:38 -0300

    Constantes de Erros em PHP

---
+++ erros-excecoes/constantes-erros.php
@@ -0,0 +1,16 @@
+<?php
+echo "<h1>Constantes Pré-definidas de Erros</h1>";
+
+var_dump(E_ERROR);
+echo "</br>";
+var_dump(E_WARNING);
+echo "</br>";
+var_dump(E_NOTICE);
+echo "</br>";
+var_dump(E_CORE_ERROR);
+echo "</br>";
+var_dump(E_STRICT);
+echo "</br>";
+var_dump(E_DEPRECATED);
+echo "</br>";
+var_dump(E_ALL);
