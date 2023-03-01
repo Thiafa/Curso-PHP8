c520a03e31c792c3bcf27ccd2b768efc363736ef
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-03-01 14:17:25 -0300

    Variavel Pré Definida  em PHP

---
+++ variaveis-pre-definidas/server.php
@@ -0,0 +1,12 @@
+<pre>
+
+<?php
+echo "<h1>Variavel Pré-Definida \$_SERVER</h1>";
+
+var_dump($_SERVER["SERVER_SOFTWARE"]);
+var_dump($_SERVER["REQUEST_URI"]);
+var_dump($_SERVER["DOCUMENT_ROOT"]);
+var_dump($_SERVER["PHP_SELF"]);
+
+?>
+</pre>
