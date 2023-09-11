088b5b44ad58eb3a1a8b68162de9e0b24b8b6d27
Thiago <gerencia.desenvolvimento@unimontes.br>
2023-09-11 17:23:33 -0300

    Number format - string

---
+++ string/number_format.php
@@ -0,0 +1,9 @@
+<?php
+
+echo number_format(5000.79, 1);
+echo "     ";
+echo number_format(5000.79, 2);
+echo "     ";
+echo number_format(5000.79, 5);
+echo "     ";
+echo number_format(5000.79, 2, ",", ".");
