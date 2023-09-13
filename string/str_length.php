e66781807138b0935f80f1c9ca984776eb6da014
Thiago <gerencia.desenvolvimento@unimontes.br>
2023-09-13 13:56:17 -0300

    string length in php

---
+++ string/str_length.php
@@ -0,0 +1,6 @@
+<?php
+
+$msg = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam adipisci consequatur cum harum, iusto sed omnis minus earum. Explicabo consequatur itaque autem odio laboriosam nam saepe eligendi aspernatur sequi mollitia!";
+
+echo strlen($msg) . " caracteres.\n";
+echo str_word_count($msg) . " palavras.\n";
